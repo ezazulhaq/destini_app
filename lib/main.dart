4f38484dc8f7f3d931ab1fd985f2bcee11203304
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-05-20 23:32:00 +0530

    Add background.png to this Container as a background image.

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //TODO: Step 1 - Add background.png to this Container as a background image. - done
+        //Step 1 - Add background.png to this Container as a background image. - done
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("images/background.png"),
